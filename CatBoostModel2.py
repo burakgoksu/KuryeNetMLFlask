d2d9dcfba0fe46fe0d1f29a9aac5025f57b8ed30
burakgoksu <gogsub58365@gmail.com>
2024-04-09 00:08:13 +0300

    KuryeNet CatBoost Model

---
+++ CatBoostModel2.py
@@ -0,0 +1,83 @@
+from catboost import CatBoostRegressor
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+
+# Veri setini yükle
+df = pd.read_csv('KaggleData.csv')
+
+# Bağımsız değişkenler (X) ve bağımlı değişken (y) seçimi
+X = df[['Delivery_Person_Age', 'Weather_Condition', 'Road_Traffic_Density', 'Type_Of_Order', 'Type_Of_Vehicle', 'Multiple_Deliveries', 'City', 'Distance', 'Day_Type', 'Time_Category']]
+y = df['Time_Taken']
+
+# Kategorik özellikleri sayısal hale getir
+X = pd.get_dummies(X)
+
+# Eğitim ve test setlerine ayırma
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# CatBoost modelini oluştur
+cb_model = CatBoostRegressor(iterations=1000, learning_rate=0.01, depth=8, loss_function='RMSE', verbose=100,l2_leaf_reg= 1)
+
+# Kategorik özelliklerin indekslerini bul
+cat_features = np.where(X_train.dtypes == 'object')[0]
+
+# Modeli eğit
+cb_model.fit(X_train, y_train, cat_features=cat_features)
+
+# Test seti üzerinde tahminler yap
+predictions = cb_model.predict(X_test)
+
+# Modelin performansını değerlendir
+rmse = mean_squared_error(y_test, predictions, squared=False)
+r2 = r2_score(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
+print(f"MSE: {mse}")
+print(f"RMSE: {rmse}")
+print(f"R^2: {r2}")
+
+# Modeli kaydet
+cb_model.save_model('catboost_model.cbm')
+
+
+# Modeli yükle
+loaded_cb_model = CatBoostRegressor()
+loaded_cb_model.load_model('catboost_model.cbm')
+
+
+# Yeni bir veri noktası oluştur
+new_data = {
+    'Delivery_Person_Age': [25],
+    'Weather_Condition': ['Cloudy'],
+    'Road_Traffic_Density': ['Low'],
+    'Type_Of_Order': ['Meal'],
+    'Type_Of_Vehicle': ['Car'],
+    'Multiple_Deliveries': [0],
+    'City': ['Metropolitian'],
+    'Distance': [18938],
+    'Day_Type': ['Weekday'],
+    'Time_Category': ['Evening']
+}
+
+# Yeni veri noktasını DataFrame'e dönüştür
+new_data_df = pd.DataFrame(new_data)
+
+# Modelin eğitildiği veri setindeki kategorik özellikler için one-hot encoding uygula
+# Burada, `X` model eğitiminde kullanılan özelliklerin orijinal DataFrame'idir.
+# `pd.get_dummies` fonksiyonunu kullanarak, eğitim setindeki kategorik özelliklere uygulanan one-hot encoding işlemini yeni veriye de uygulayabiliriz.
+new_data_processed = pd.get_dummies(new_data_df)
+# Eğitim setindeki tüm one-hot encoded sütunları elde etmek için kullanılan kod:
+X_encoded_columns = pd.get_dummies(X).columns
+# Yeni veride eksik olan sütunları sıfır değeri ile doldur:
+for col in X_encoded_columns:
+    if col not in new_data_processed.columns:
+        new_data_processed[col] = 0
+# Sütun sırasını eğitim seti ile aynı hale getir
+new_data_processed = new_data_processed[X_encoded_columns]
+
+# Modeli kullanarak yeni veri noktası için tahmin yap
+prediction = loaded_cb_model.predict(new_data_processed)
+
+print(f"Tahmini Süre: {prediction[0]} dakika")
+
---
+++ app.py
@@ -0,0 +1,129 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, jsonify
+import pandas as pd
+from flask_httpauth import HTTPBasicAuth
+from CatBoostModel import KuryeNetML
+
+
+
+app = Flask(__name__)
+
+# Loglama için temel yapılandırmayı ayarla
+logging.basicConfig(level=logging.INFO)
+
+# Rotating log dosyaları oluştur, 1MB'da bir yeni dosya oluştur ve en fazla 10 dosya sakla
+file_handler = RotatingFileHandler('KuryeNetApp.log', maxBytes=1024 * 1024, backupCount=10)
+file_handler.setLevel(logging.INFO)  # INFO ve üzeri seviyedeki logları yakala
+
+# Log mesajları için bir format belirle
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Flask app logger'ına file handler'ı ekle
+app.logger.addHandler(file_handler)
+
+auth = HTTPBasicAuth()
+
+kuryeNetML = KuryeNetML('AllData.csv')
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username == 'admin' and password == 'admin':
+        app.logger.info(f"Method: verify_password, Username: {username}, Password: {password}, Basic Auth successful.")
+        return True
+    else:
+        app.logger.error(f"Method: verify_password, Username: {username}, Password: {password}, Basic Auth failed.")
+        return False
+
+
+@app.route('/predict', methods=['POST'])
+@auth.login_required
+def predict():
+    try:
+
+        # JSON olarak gönderilen veriyi al
+        data = request.json
+        print(data)
+
+        Delivery_Person_Age = data.get('delivery_Person_Age',25)
+        print(Delivery_Person_Age)
+        Weather_Condition = data.get('weather_Condition','Sunny')
+        Road_Traffic_Density = data.get('road_Traffic_Density','Low')
+        Type_Of_Order = data.get('type_Of_Order','Meal')
+        Type_Of_Vehicle = data.get('type_Of_Vehicle','Motorcycle')
+        Multiple_Deliveries = data.get('multiple_Deliveries',0)
+        City = data.get('city','Metropolitian')
+        Distance = data.get('distance',0)
+        Day_Type = data.get('day_Type','Weekday')
+        Time_Category = data.get('time_Category','Noon')
+
+        prediction_value = kuryeNetML.Prediction(Delivery_Person_Age,Weather_Condition,Road_Traffic_Density,Type_Of_Order,Type_Of_Vehicle,Multiple_Deliveries,City,Distance,Day_Type,Time_Category)
+
+        app.logger.info(f"Method: predict, Data: {data}, Prediction Value: {prediction_value}, Rounded: {round(prediction_value)}, Prediction request successful.")
+        return jsonify({'Prediction':prediction_value,
+                        'PredictionRound':round(prediction_value)})
+
+    except Exception as e:
+        app.logger.error(f"Method: predict, Error: {str(e)}, Prediction request unsuccessful.")
+
+        return jsonify({'error': str(e)})
+
+
+@app.route('/write_data', methods=['POST'])
+@auth.login_required
+def write_data():
+    try:
+        # JSON olarak gönderilen veriyi al
+        data = request.json
+        # Veriyi DataFrame'e dönüştür (tek bir nesne için bir liste içinde)
+        data_df = pd.DataFrame([data])  # Anahtarları sütun adlarına, değerleri ise satır değerlerine dönüştürür
+
+        # Dosyanın var olup olmadığını kontrol et ve buna göre header'ı ayarla
+        file_exists = os.path.exists('KuryeNetData.csv')
+
+        # Veriyi CSV dosyasına yaz
+        # Eğer dosya yoksa, header yazılır; varsa, yazılmaz
+        data_df.to_csv('KuryeNetData.csv', mode='a', header=not file_exists, index=False)
+        app.logger.info(f"Method: write_data , Data: {data}, Data was written successful on KuryeNetData.csv file.")
+        return jsonify({'message': 'Data successfully written to KuryeNetData.csv'})
+    except Exception as e:
+        app.logger.error(f"Method: write_data, Error: {str(e)}, Data unsuccessfully written to KuryeNetData.csv.")
+        return jsonify({'error': str(e)})
+
+
+
+@app.route('/retrain_model', methods=['POST'])
+@auth.login_required
+def retrain_model():
+    try:
+        # Yeni verileri içeren CSV dosyasını kontrol et
+        new_data_path = 'KuryeNetData.csv'
+        if not os.path.exists(new_data_path):
+            app.logger.error(f"Method: retrain_model, Error: {new_data_path}, KuryeNetData file does not exist.")
+            return jsonify({'error': 'KuryeNetData file does not exist.'})
+
+        # İlk CSV dosyasını oku
+        df1 = pd.read_csv('KaggleData2.csv')
+
+        # İkinci CSV dosyasını oku
+        df2 = pd.read_csv('KuryeNetData.csv')
+
+        # İki DataFrame'i satır bazında birleştir
+        birlesik_df = pd.concat([df1, df2], ignore_index=True)
+
+        # Birleştirilmiş DataFrame'i yeni bir CSV dosyasına yaz
+        birlesik_df.to_csv('AllData.csv', index=False)
+
+        kuryeNetML.TrainCatBoostModel()
+
+        app.logger.info(f"Method: retrain_model, Model: catboost_model2.cbm, Model has been retrained with All Data.")
+        return jsonify({'message': 'Model has been retrained with All Data.'})
+    except Exception as e:
+        app.logger.error(f"Method: retrain_model, Error: {str(e)}, Model has not been retrained with All Data.")
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
